3f7e1af6cf20ae6c183fc07f9e2d61166055624b
Albert Vaca Cintora <albert.vaca@datadoghq.com>
2022-11-17 12:59:10 +0100

    unittest test

---
+++ tests/test_it.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestFoo(unittest.TestCase):
+    def test_whatever(self):
+        pass
+
+
+class TestBar(unittest.TestCase):
+    def test_whatever(self):
+        pass
+
